b3eac61783ea6783b50bc4a2b40cfbdff8ab678e
Tatyana Guzovskaya <guzovskaya.tanya.gogle@gmail.com>
2026-02-03 17:05:13 +0300

    updated coor

--- Assets/Scripts/Coordinate.cs
+++ Assets/Scripts/Coordinate.cs
@@ -2,6 +2,8 @@ public class Coordinate
 {
     public int Row { get; set; }
     public int Col { get; set; }
+    public int Coor { get; set; }
+    public int cool { get; set; }
     public Coordinate(int row, int col)
     {
         Row = row;
